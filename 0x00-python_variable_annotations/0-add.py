9bef80ea9ee29c2c9d40e322184be4fe4292e030
Samson Manie <samsonmanie2022@gmail.com>
2024-08-02 10:29:24 +0000

    annotated functions

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def add(a: float, b:float) -> float:
+    """
+    adds two floating number and returns sum
+
+    param a: is the first floating point number
+    param b: is the second floating point number
+    """
+    return a+b

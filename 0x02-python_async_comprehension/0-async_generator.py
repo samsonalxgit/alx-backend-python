583292d2284cf985941ded7f61d0328bc447b7b2
Samson Manie <samsonmanie2022@gmail.com>
2024-08-06 19:59:55 +0000

    async compression

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 '''Task 0's module.
-'''import asyncio
+'''
+import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
-
-# To demonstrate how it works, we can write an async function to consume the generator.
-async def main():
-    async for value in async_generator():
-        print(value)
-
-# Run the main function to see the output
-asyncio.run(main())
+        yield random.random() * 10
